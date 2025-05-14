70b67d0cde75499c485baabf43fbee1bcf6ea214
Adrian Duran Madrigal <duranmadrigala@gmail.com>
2025-05-13 22:28:26 -0600

    actualizacion clase cliente

--- TCPClient.java
+++ TCPClient.java
@@ -1,1 +1,33 @@
-// cambio a java
+
+import java.io.*;
+import java.net.*;
+
+public class TCPClient {
+    public static void main(String[] args) throws Exception {
+        BufferedReader userInputReader = new BufferedReader(
+                new InputStreamReader(System.in));
+        
+        Socket socketConnection = new Socket("mi-host", "6789");
+        
+        
+        DataOutputStream outputWriter = new DataOutputStream(
+                socketConnection.getInputStream());
+        
+        BufferedReader serverResponseReader = new BufferedReader(
+                new InputStreamReader(socketConnection.getInputStream()));
+
+        String line;
+        System.out.println("Ingrese texto (EXIT para terminar):");
+        while ((line = userInputReader.readLine()) != null) {
+            outputWriter.writeBytes(line + "\n");
+            if (line.equals("EXIT")) {
+                break; 
+            }
+            String response = serverResponseReader.readLine; 
+            System.out.println("RESPUESTA DEL SERVIDOR:\n" + response);
+        }
+
+        socketConnection.close;  
+        userInputReader.close();
+    }
+}
