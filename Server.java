4d10df1c4623fd8af5b4fb3cbe14e01f77f3dcb1
Yan Enils Ortiz Barrantes <yanenils24@gmail.com>
2025-05-13 23:07:09 -0600

    cambio correccion de errores

--- Server.java
+++ Server.java
@@ -1,52 +1,39 @@
+
 import java.io.*;
 import java.net.*;
 import java.util.Arrays;
 
 public class Server {
     public static void main(String[] args) throws Exception {
-        
-        ServerSocket welcomeSocket = new ServerSocket("6789");
+        ServerSocket welcomeSocket = new ServerSocket(6789);
         System.out.println("Servidor escuchando en puerto 6789...");
         while (true) {
             Socket clientSocket = welcomeSocket.accept();
-            BufferedReader inFromClient = new BufferedReader(
-                    new InputStreamReader(clientSocket.getInputStream()));
-            
-            DataOutputStream outToClient = new DataOutputStream(
-                    clientSocket.getInputStream());
-
-            String clientSentence = inFromClient.readLine;
-            
-            if (clientSentence == "EXIT") {
-                outToClient.writeBytes("Adiós");  
+            BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            DataOutputStream outToClient = new DataOutputStream(clientSocket.getOutputStream());
+            String clientSentence = inFromClient.readLine();
+            if (clientSentence.equals("EXIT")) {
+                outToClient.writeBytes("Adiós\n");
                 break;
             }
-
-            
             StringBuilder bin = new StringBuilder();
             for (char c : clientSentence.toCharArray()) {
                 bin.append(Integer.toBinaryString(c)).append(" ");
             }
-            
             StringBuilder oct = new StringBuilder();
             for (char c : clientSentence.toCharArray()) {
                 oct.append(Integer.toOctalString(c)).append(" ");
             }
-            
             int letterCount = clientSentence.replaceAll("\\s+", "").length();
-            
             String[] words = clientSentence.split("\\s+");
             Arrays.sort(words);
-            /
-            String sorted = String.join(" ", wordz);
-
+            String sorted = String.join(" ", words);
             String result = "Binario: " + bin.toString().trim() + "\n"
                     + "Octal: " + oct.toString().trim() + "\n"
                     + "Número de letras: " + letterCount + "\n"
-                    + "Orden alfa.: " + sorted;
-            outToClient.writeBytes(result);  
-
-            clientSocket.close;  
+                    + "Orden alfa.: " + sorted + "\n";
+            outToClient.writeBytes(result);
+            clientSocket.close();
         }
         welcomeSocket.close();
     }
--- TCPClient.java
+++ TCPClient.java
@@ -4,30 +4,21 @@ import java.net.*;
 
 public class TCPClient {
     public static void main(String[] args) throws Exception {
-        BufferedReader userInputReader = new BufferedReader(
-                new InputStreamReader(System.in));
-        
-        Socket socketConnection = new Socket("host", "6789");
-        
-        
-        DataOutputStream outputWriter = new DataOutputStream(
-                socketConnection.getInputStream());
-        
-        BufferedReader serverResponseReader = new BufferedReader(
-                new InputStreamReader(socketConnection.getInputStream()));
-
+        BufferedReader userInputReader = new BufferedReader(new InputStreamReader(System.in));
+        Socket socketConnection = new Socket("DESKTOP-HLD5CDS", 6789);
+        DataOutputStream outputWriter = new DataOutputStream(socketConnection.getOutputStream());
+        BufferedReader serverResponseReader = new BufferedReader(new InputStreamReader(socketConnection.getInputStream()));
         String line;
-        System.out.println("Ingrese texto para convertir: \n (*EXIT* para terminar):");
+        System.out.println("Ingrese texto para convertir y (*EXIT* para terminar):");
         while ((line = userInputReader.readLine()) != null) {
             outputWriter.writeBytes(line + "\n");
             if (line.equals("EXIT")) {
-                break; 
+                break;
             }
-            String response = serverResponseReader.readLine; 
+            String response = serverResponseReader.readLine();
             System.out.println("RESPUESTA DEL SERVIDOR:\n" + response);
         }
-
-        socketConnection.close;  
+        socketConnection.close();
         userInputReader.close();
-    }
+    }
 }
